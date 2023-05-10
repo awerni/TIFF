ed8ab5e74a1dc26433ac906d45c50caaed8e3bb1
Andreas Wernitznig <andreas@wernitznig.com>
2023-05-10 12:55:37 +0200

    removed matomo

--- R/mod_app.R
+++ R/mod_app.R
@@ -28,19 +28,6 @@ appUI <- function(settings, proliferationTests, hallmarkGeneSets) {
     tags$script(src = withHash(filePath = "www/main.js", wwwPath = "main.js")),
     HTML(
       '<!-- Matomo -->
-      <script type="text/javascript">
-        var _paq = window._paq || [];
-        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-        _paq.push(["trackPageView"]);
-        _paq.push(["enableLinkTracking"]);
-        (function() {
-          var u="//bi-bio-matomo.aws.boehringer.com/";
-          _paq.push(["setTrackerUrl", u+"matomo.php"]);
-          _paq.push(["setSiteId", "2"]);
-          var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0];
-          g.type="text/javascript"; g.async=true; g.defer=true; g.src=u+"matomo.js"; s.parentNode.insertBefore(g,s);
-        })();
-      </script>
       <!-- End Matomo Code -->'
     )
   )
