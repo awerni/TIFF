a8e6202cd80e88342ab893c67b23a11686dcc47b
Andreas Wernitznig <andreas@wernitznig.com>
2023-05-10 12:56:20 +0200

    moved from broad to PF

--- inst/deploy/deploy.sh
+++ inst/deploy/deploy.sh
@@ -5,5 +5,5 @@ pwd
 cp Docker/Dockerfile /nas1/.scratch/TIFF/
 cp Docker/Rprofile.site /nas1/.scratch/TIFF/
 cp Docker/install_prereqs_tiff.R /nas1/.scratch/TIFF/
-ssh charlotte "cd /nas1/.scratch/TIFF;docker build . --no-cache -t tiff/broadinstitute.org"
-#ssh charlotte "cd /nas1/.scratch/TIFF;docker build . -t tiff/broadinstitute.org"
+ssh charlotte "cd /nas1/.scratch/TIFF;docker build . --no-cache -t tiff/pierre-fabre.net"
+#ssh charlotte "cd /nas1/.scratch/TIFF;docker build . -t tiff/pierre-fabre.net"
